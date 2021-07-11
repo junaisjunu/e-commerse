12b443ad121962273cf0cfe2ed0c4ab38e823874
junais <junaisk456@gmail.com>
2021-07-11 16:58:32 +0530

    using withot js file

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products =[
+  {
+    name:"samsung",
+    category:"mobile",
+    discription:"good phone",
+    Image :"https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s20-5g-r1.jpg"
+    
+
+  },
+  {
+    name:"oppo",
+    category:"mobile",
+    discription:"good phone",
+    Image :"https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s20-5g-r1.jpg"
+    
+
+  },
+  {
+    name:"iphone",
+    category:"mobile",
+    discription:"good phone",
+    Image :"https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s20-5g-r1.jpg"
+    
+
+  },{
+    name:"lenova",
+    category:"mobile",
+    discription:"good phone",
+    Image :"https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-s20-5g-r1.jpg"
+    
+
+  }
+  
+  ]
+  res.render('index', { products});
+});
+
+module.exports = router;
